2466fb366a3f574c5d82e6440c8a3d3153d278a2
nitecse <hitman12457868@gmail.com>
2024-10-14 19:21:14 +0530

    fixed some test cases

--- src/App.js
+++ src/App.js
@@ -3,74 +3,104 @@ import React, {useEffect} from "react";
 import './App.css';
 
 function App() {
-  const [openForm, setOpenForm] = React.useState(false);
-  const [email, setEmail] = React.useState('');
-  const [username, setUsername] = React.useState('');
-  const [phone, setPhone] = React.useState('');
-  const [dob, setDob] = React.useState('');
+	const [openForm, setOpenForm] = React.useState(false);
+	const [email, setEmail] = React.useState('');
+	const [username, setUsername] = React.useState('');
+	const [phone, setPhone] = React.useState('');
+	const [dob, setDob] = React.useState('');
 
-  const handleInput = () => {
-    let d = new Date(dob);
-    let c = new Date();
-    if(!email.includes('@')){alert("Invalid email. Please enter a 10-digit phone number."); return false;}
-    if(phone.length !== 10) {alert("Invalid phone number. Please enter a 10-digit phone number."); return false;}
-    if(d>c){alert("Invalid date of birth. Please enter a 10-digit phone number."); return false;}
-    if(email === '' || username === '' || phone === '' || dob === ''){alert("Please enter all details first."); return false;}
-    return true;
-  }
+	let modalRef = React.useRef(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if(handleInput())
-    {
-      setOpenForm(false);
-      setEmail('');
-      setUsername('');
-      setPhone('');
-      setDob('');
-    }
-  }
+	const handleInput = () => {
+		let d = new Date(dob);
+		let c = new Date();
+		if (!email.includes('@')) {
+			alert("Invalid email. Please enter a 10-digit phone number.");
+			return false;
+		}
+		if (phone.length !== 10) {
+			alert("Invalid phone number. Please enter a 10-digit phone number.");
+			return false;
+		}
+		if (d > c) {
+			alert("Invalid date of birth. Please enter a 10-digit phone number.");
+			return false;
+		}
+		if (email === '' || username === '' || phone === '' || dob === '') {
+			alert("Please enter all details first.");
+			return false;
+		}
+		return true;
+	}
 
-  useEffect(() => {
-    let modal = document.querySelector(".modal");
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (handleInput()) {
+			setOpenForm(false);
+			setEmail('');
+			setUsername('');
+			setPhone('');
+			setDob('');
+		}
+	}
 
-    if(!openForm) {
-      modal.style.backgroundColor = "rgba(255, 255, 255, 1)";
-    }
-    else {
-      modal.style.backgroundColor = "rgb(0, 0, 0, 0.5)";
-    }
-  }, [openForm]);
+	const handleOutsideClick = (e) => {
+		if(modalRef.current && !modalRef.current.contains(e.target)){
+			setOpenForm(false);
+		}
+	}
+
+	useEffect(() => {
+		let modal = document.querySelector(".modal");
+
+		if (!openForm) {
+			modal.style.backgroundColor = "rgba(255, 255, 255, 1)";
+		} else {
+			modal.style.backgroundColor = "rgb(0, 0, 0, 0.5)";
+			document.addEventListener('mousedown', handleOutsideClick);
+		}
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+		}
+	}, [openForm]);
 
 
-  return (
-    <div id='modal' className="modal App">
-      <h1>User Details Modal</h1>
-      <button onClick={()=>setOpenForm(true)}>Open Form</button>
-      {
-        openForm && (
-              <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                <button className='modal-close' onClick={() => setOpenForm(false)}>x</button>
-                <form onSubmit={(e) => {
-                  handleSubmit(e);
-                }}>
-                  <h3>Fill Details</h3>
-                  <h5>Username: </h5>
-                  <input type="text" onChange={(e) => setUsername(e.target.value)} id='username'></input>
-                  <h5>Email Address: </h5>
-                  <input type="text" onChange={(e) => setEmail(e.target.value)} id='email'></input>
-                  <h5>Phone Number: </h5>
-                  <input type="text" onChange={(e) => setPhone(e.target.value)} id='phone'></input>
-                  <h5>Date of Birth: </h5>
-                  <input type="date" onChange={(e) => setDob(e.target.value)} id='dob'></input>
-                  <h5 />
-                  <button type="submit" className='submit-button'>Submit</button>
-                </form>
-              </div>
-          )
-      }
-    </div>
-  );
+	return (
+		<div id='modal' className="modal">
+			<h1>User Details Modal</h1>
+			<button onClick={() => setOpenForm(true)}>Open Form</button>
+			{
+				openForm && (
+					<div className="modal-content" ref={modalRef} onClick={(e) => handleOutsideClick(e)}>
+						<button className='modal-close' onClick={() => setOpenForm(false)}>x</button>
+						<form onSubmit={(e) => {
+							handleSubmit(e);
+						}}>
+							<h3>Fill Details</h3>
+							<div className='same-line'>
+								<p>Username: </p>
+								<input type="text" onChange={(e) => setUsername(e.target.value)} id='username'></input>
+							</div>
+							<div className='same-line'>
+								<p>Email Address: </p>
+								<input type="text" onChange={(e) => setEmail(e.target.value)} id='email'></input>
+							</div>
+							<div className='same-line'>
+								<p>Phone Number: </p>
+								<input type="text" onChange={(e) => setPhone(e.target.value)} id='phone'></input>
+							</div>
+							<div className='same-line'>
+								<p>Date of Birth: </p>
+								<input type="date" onChange={(e) => setDob(e.target.value)} id='dob'></input>
+							</div>
+							<p/>
+							<button type="submit" className='submit-button'>Submit</button>
+						</form>
+					</div>
+				)
+			}
+		</div>
+	);
 }
 
 export default App;
